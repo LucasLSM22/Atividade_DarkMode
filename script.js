5cc00d4dd33c440c5ffec2ccd52429ca8acd0b6c
Lucas Marçal <126588175+LucasLSM22@users.noreply.github.com>
2023-10-02 16:59:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,8 @@ function ModoEscuro(button) {
   square.style.backgroundColor = "#000000";
   square.style.color = "white";
 
-  var x = document.getElementById("txtNome");
-  x.style.backgroundColor = "#1C1C1C";
-  x.style.color = "white";
-
-  var x = document.getElementById("txtTelefone");
-  x.style.backgroundColor = "#1C1C1C";
-  x.style.color = "white";
-
-  var x = document.getElementById("txtOrigem");
-  x.style.backgroundColor = "#1C1C1C";
-  x.style.color = "white";
-
-  var x = document.getElementById("txtDataContato");
-  x.style.backgroundColor = "#1C1C1C";
-  x.style.color = "white";
-
-  var x = document.getElementById("txtObservacao");
-  x.style.backgroundColor = "#1C1C1C";
-  x.style.color = "white";
+  var ModoEscuroAtivo = document.getElementById("txtNome", "txtTelefone", "txtOrigem", "txtDataContato", "txtObservacao" );
+  ModoEscuroAtivo.style.backgroundColor = "#1C1C1C";
+  ModoEscuroAtivo.style.color = "white";
 }
+
