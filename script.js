91f948f92b9310f8bef2bb295a4f5b064f701a95
Lucas Marçal <126588175+LucasLSM22@users.noreply.github.com>
2023-10-02 16:55:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function ModoEscuro(button) {
+  var square = document.getElementById("square");
+  square.style.backgroundColor = "#000000";
+  square.style.color = "white";
+
+  var x = document.getElementById("txtNome");
+  x.style.backgroundColor = "#1C1C1C";
+  x.style.color = "white";
+
+  var x = document.getElementById("txtTelefone");
+  x.style.backgroundColor = "#1C1C1C";
+  x.style.color = "white";
+
+  var x = document.getElementById("txtOrigem");
+  x.style.backgroundColor = "#1C1C1C";
+  x.style.color = "white";
+
+  var x = document.getElementById("txtDataContato");
+  x.style.backgroundColor = "#1C1C1C";
+  x.style.color = "white";
+
+  var x = document.getElementById("txtObservacao");
+  x.style.backgroundColor = "#1C1C1C";
+  x.style.color = "white";
+}
